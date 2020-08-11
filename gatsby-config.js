61e38f8a18a4341fbf6eb02e43b74f98585a9b28
Russ Wells <russ@russdigital.com>
2020-08-10 20:10:55 -0700

    inital

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Hyperspace Devblog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Russell Wells`,
+      summary: `dev blog of hyperspace design, media & technology.`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
+    description: `dev blog of hyperspace design, media & technology.`,
+    siteUrl: `https://blog.hyperspace.im/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `russellwells`,
     },
   },
   plugins: [
